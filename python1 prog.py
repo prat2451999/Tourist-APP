35f34ed7b0a2cbc1990f9f803cb57c9312b5de1e
prat2451999 <76038133+prat2451999@users.noreply.github.com>
2022-01-12 22:22:14 +0530

    added new file

---
+++ python1 prog.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+a=input("enter your name")
+b=a+ " singh"
+print('your full name is :',b)
+
+
+
+# In[11]:
+
+
+print("hello this is my python program")
+
+
+# In[15]:
+
+
+a=int(25.89)
+print(a)
+
+
+# In[ ]:
+
+
+a=int(input("enter your name"))
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+a=int(input("enter your name"))
+print(a)
+
+
+# In[ ]:
+
+
+
+
